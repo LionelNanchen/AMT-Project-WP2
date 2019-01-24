637761d315964ea77f9dbef19cd870443a58353c
kam_ <kamil.amrani@heig-vd.ch>
2019-01-24 18:23:38 +0100

    moved cucumber to new project

--- cucumber-test/test/ch/heigvd/gamification/api/spec/steps/BadgesSteps.java
+++ cucumber-test/test/ch/heigvd/gamification/api/spec/steps/BadgesSteps.java
@@ -1,32 +1,29 @@
 package ch.heigvd.gamification.api.spec.steps;
 
-import ch.heigvd.gamification.api.BadgesApi;
-import ch.heigvd.gamification.api.BadgesController;
-
+import ch.heigvd.gamification.api.DefaultApi;
 import ch.heigvd.gamification.api.dto.BadgesDTO;
 import ch.heigvd.gamification.api.dto.BadgesResponseDTO;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.When;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public class BadgesSteps {
     private UtilsSteps utilsSteps = new UtilsSteps();
-    private BadgesApi badgesApi = new BadgesController();
+    private DefaultApi defaultApi = new DefaultApi();
 
     @When("^I try to POST a new badge$")
     public void i_try_to_POST_a_new_badge() throws Throwable {
         BadgesDTO badges = new BadgesDTO();
         badges.setName("test");
         badges.setDescription("a test");
-        badgesApi.badgesPost(utilsSteps.getApiKey(), badges);
+        defaultApi.badgesPost(utilsSteps.getApiKey(), badges);
     }
 
     @When("^I GET all the badges from the endpoint /badges$")
     public void i_GET_all_the_badges_from_the_endpoint_badges() throws Throwable {
-        ResponseEntity<List<BadgesResponseDTO>> res = badgesApi.badgesGet(utilsSteps.getApiKey());
-        utilsSteps.setStatusCode(res.getStatusCodeValue());
+        List<BadgesResponseDTO> res = defaultApi.badgesGet(utilsSteps.getApiKey());
+        utilsSteps.setStatusCode(res.size());
     }
 
     @When("^I try to DELETE the badge$")
