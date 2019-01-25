d9b33df47b6efa320f9ea9093856fd8f81f96218
kam_ <kamil.amrani@heig-vd.ch>
2019-01-25 10:13:00 +0100

    impl event tests

--- cucumber-test/test/ch/heigvd/gamification/api/spec/steps/EventsSteps.java
+++ cucumber-test/test/ch/heigvd/gamification/api/spec/steps/EventsSteps.java
@@ -1,34 +1,94 @@
 package ch.heigvd.gamification.api.spec.steps;
 
+import ch.heigvd.gamification.ApiException;
+import ch.heigvd.gamification.ApiResponse;
 import ch.heigvd.gamification.api.DefaultApi;
+import ch.heigvd.gamification.api.dto.*;
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
+import org.joda.time.DateTime;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class EventsSteps {
     private DefaultApi defaultApi = new DefaultApi();
-    private long id;
+    private int ruleId;
+    private long badgeId;
+    private long pointScaleId;
+
+    @And("^I have a badge$")
+    public void iHaveABadge() throws Throwable {
+        BadgesDTO badges = new BadgesDTO();
+        badges.setName("test 2");
+        badges.setDescription("a test 2");
+        ApiResponse<Void> res = defaultApi.badgesPostWithHttpInfo(UtilsSteps.getApiKey(), badges);
+        badgeId = Long.parseLong(res.getHeaders().get("id").get(0));
+    }
+
+    @And("^I have a pointScale$")
+    public void iHaveAPointScale() throws Throwable {
+        PointScaleDTO pointScale = new PointScaleDTO();
+        pointScale.setPointScaleName("test 2");
+        ApiResponse<Void> res = defaultApi.pointScalePostWithHttpInfo(UtilsSteps.getApiKey(), pointScale);
+        pointScaleId = Long.parseLong(res.getHeaders().get("id").get(0));
+    }
+
+    @And("^I have a rule$")
+    public void iHaveARule() throws Throwable {
+        RuleDTO ruleDTO = RulesSteps.createTestRuleDTO(badgeId, pointScaleId);
+        ApiResponse<Void> res = defaultApi.rulesPostWithHttpInfo(UtilsSteps.getApiKey(), ruleDTO);
+        UtilsSteps.setStatusCode(res.getStatusCode());
+        ruleId = Integer.parseInt(res.getHeaders().get("id").get(0));
+    }
 
     @When("^I try to POST a new event$")
     public void i_try_to_POST_a_new_event() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        EventDTO eventDTO= new EventDTO();
+        List<ValueDTO> properties = new LinkedList<>();
+        ValueDTO valueDTO = new ValueDTO();
+        valueDTO.setKey("key");
+        valueDTO.setValue("3");
+
+        eventDTO.setProperties(properties);
+        eventDTO.setRemoteUserId("1");
+        eventDTO.setTimestamp(new DateTime("2019-01-25T08:04:49.694Z"));
+        eventDTO.setType("test");
+
+        try {
+            ApiResponse<EventDTO> res = defaultApi.reportEventWithHttpInfo(UtilsSteps.getApiKey(), eventDTO);
+            UtilsSteps.setStatusCode(res.getStatusCode());
+        } catch (ApiException e) {
+            UtilsSteps.setStatusCode(e.getCode());
+        }
+
+        iHaveDeletedARule();
+        iHaveDeletedABadge();
+        iHaveDeletedAPointScale();
     }
 
-    @When("^I GET all the badges from the endpoint /events$")
-    public void i_GET_all_the_badges_from_the_endpoint_events() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    private void iHaveDeletedARule() throws Throwable {
+        try {
+            defaultApi.rulesIdDeleteWithHttpInfo(UtilsSteps.getApiKey(), ruleId);
+        } catch (ApiException e) {
+            System.err.println(e.getCode());
+        }
     }
 
-    @When("^I try to DELETE the event$")
-    public void i_try_to_DELETE_the_event() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    private void iHaveDeletedABadge() throws Throwable {
+        try {
+            defaultApi.badgesIdDeleteWithHttpInfo(UtilsSteps.getApiKey(), badgeId);
+        } catch (ApiException e) {
+            System.err.println(e.getCode());
+        }
     }
 
-    @When("^I try to change the event's name$")
-    public void i_try_to_change_the_event_s_name() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    private void iHaveDeletedAPointScale() throws Throwable {
+        try {
+            defaultApi.pointScaleIdDeleteWithHttpInfo(UtilsSteps.getApiKey(), pointScaleId);
+        } catch (ApiException e) {
+            System.err.println(e.getCode());
+        }
     }
 }
--- cucumber-test/test/resources/scenarios/event.feature
+++ cucumber-test/test/resources/scenarios/event.feature
@@ -1,33 +1,9 @@
 Feature: Events
 
-  Scenario: Get all events
+  Scenario: Post an event
     Given I have an api key
+    And I have a badge
+    And I have a pointScale
+    And I have a rule
     When I try to POST a new event
     Then I receive status code <201>
-    When I GET all the badges from the endpoint /events
-    Then I receive status code <200>
-    When I try to DELETE the event
-    Then I receive status code <204>
-
-  Scenario: Create a new event
-    Given I have an api key
-    When I try to POST a new event
-    Then I receive status code <201>
-    When I try to DELETE the event
-    Then I receive status code <204>
-
-  Scenario: Delete a event
-    Given I have an api key
-    When I try to POST a new event
-    Then I receive status code <201>
-    When I try to DELETE the event
-    Then I receive status code <204>
-
-  Scenario: Modify a event
-    Given I have an api key
-    When I try to POST a new event
-    Then I receive status code <201>
-    When I try to change the event's name
-    Then I receive status code <200>
-    When I try to DELETE the event
-    Then I receive status code <204>
