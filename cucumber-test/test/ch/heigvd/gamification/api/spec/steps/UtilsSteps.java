e5eaff567d9abf6a67cc49cd7ef789b859013d27
kam_ <kamil.amrani@heig-vd.ch>
2019-01-25 08:47:07 +0100

    test api key validity in tests

--- cucumber-test/test/ch/heigvd/gamification/api/spec/steps/UtilsSteps.java
+++ cucumber-test/test/ch/heigvd/gamification/api/spec/steps/UtilsSteps.java
@@ -1,8 +1,14 @@
 package ch.heigvd.gamification.api.spec.steps;
 
+import ch.heigvd.gamification.ApiException;
+import ch.heigvd.gamification.ApiResponse;
+import ch.heigvd.gamification.api.DefaultApi;
+import ch.heigvd.gamification.api.dto.BadgesResponseDTO;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import org.junit.Assert;
+
+import java.util.List;
 
 public class UtilsSteps {
     private static String apiKey = "a1";
@@ -11,7 +17,17 @@ public class UtilsSteps {
     @Given("^I have an api key$")
     public void iHaveAnApiKey() {
         statusCode = -1;//invalidate last scenario code
-        Assert.assertNotEquals("", apiKey);//todo try get on an endpoint anr receive a 200
+
+        // try to get badges with apiKey to make sure apiKey is valid
+        int apiKeyStatusCode;
+        try {
+            ApiResponse<List<BadgesResponseDTO>> res = new DefaultApi().badgesGetWithHttpInfo(UtilsSteps.getApiKey());
+            apiKeyStatusCode = res.getStatusCode();
+        } catch (ApiException e) {
+            apiKeyStatusCode = e.getCode();
+        }
+
+        Assert.assertEquals(200, apiKeyStatusCode);
     }
 
     @Then("^I receive status code <(\\d+)>$")
